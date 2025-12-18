03ae98e2551ce1cb3d320d66c70409a237d0139e
YMF-UP <2571853049@qqcom>
2025-12-18 20:52:39 +0800

    测试模拟的

---
+++ src/main/java/com/practice/utils/Simulation.java
@@ -0,0 +1,22 @@
+package com.practice.utils;
+
+public class Simulation {
+
+    public static void main(String[] args) {
+
+        int m=1/3;
+        System.out.println(m);
+       /* int [] nums=new int [26];
+        char [] S=new char[26];
+        char [] s=new char[26];
+        for (int i = 0; i < 26; i++) {
+            nums[i]='a'+i-'A';
+
+        }
+        for (int i = 0; i <26; i++) {
+            S[i]= (char) ('z'-nums[25-i]);
+            System.out.println(nums[25-i]+"对应："+S[i]);
+        }*/
+
+    }
+}
