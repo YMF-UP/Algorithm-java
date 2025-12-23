cc640b7dbb296de9663f5d336dd03686a5774b9d
YMF-UP <2571853049@qqcom>
2025-12-23 16:19:57 +0800

    最长序列和---之前居然写出来了--但是现在不会了

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p128_longestConsecutive/Solution.java
@@ -0,0 +1,31 @@
+package com.practice.algorithm.leetcode.Hot100.medium.p128_longestConsecutive;
+
+
+
+
+/**
+ * LeetCode 128.
+ * 题目描述：
+ * 示例：
+ * 解题思路：
+ * 时间复杂度：O(...)
+ * 空间复杂度：O(...)
+ */
+
+
+public class Solution {
+
+    public int longestConsecutive(int[] nums) {
+        //关键点在于怎么判断---hash吗
+        /**1.没想到之前居然写出来过---虽然是c++
+         * 2.现在反而写不出来了，---果然不进反退啊
+         * */
+
+
+    }
+
+
+        public static void main(String[] args) {
+
+    }
+}
