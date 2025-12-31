02ec82d191f8b3c19f16ebab2f07300ea413a396
YMF-UP <2571853049@qqcom>
2025-12-31 11:31:14 +0800

    ai分析的很对，我一直在线性思维去做，忽略了本质的状态，所以只能一直模拟并打补丁似的去做，去修改，自然是写不出来的。真的是要长回顾了

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/hard/p42_trap/Solution.java
@@ -0,0 +1,87 @@
+package com.practice.algorithm.leetcode.Hot100.hard.p42_trap;
+
+public class Solution {
+
+    public  static int trap(int[] height) {
+        //贪心啊像
+        //高低高---得有缝隙啊---得出现起伏
+        //结束点就是碰到高于它的---但是中间怎么记录---这个最重要了
+        //先试一下吧---一个循环就行了吗？
+        //次高作为高度---宽度另算---必须高低高---出现一次高低高就计算一次---怎么感觉是递归啊
+        //头尾两个肯定不对啊
+        if(height.length<3)
+            return  0;
+        int sum=0;
+           //0,1,0,2,1,0,1,3,2,1,2,1----debug了一下，有几个问题
+          //其实就一个问题---就是提前结束---在中间就结束了
+          //但是会死循环---只能先判定了---但是那样--时间复杂度
+        for ( int right=0;right<height.length-2;) {
+//            if(height[right]==height[right-1]&&right>0)
+//                continue;
+//            if(height[right]==0)
+//                continue;
+            int left = right+1;
+            int m=height[right];//起始高
+            //确定一个区域之后再具体计算吗？
+            int sum_b=0;
+            int wide=0;//宽
+            //在这里加一个判断---用来是否整体处理
+            //但是对于0,7,1,4,6这种就解决不了了
+            while (height[right] > height[left]){
+                sum_b+=height[left];//计算高度
+                left++;
+                wide++;
+                if(left==height.length){
+                    break;
+                }
+            }
+            if(left==height.length){
+                left--;
+            }
+//            if(right==0&&left==height.length-1)
+//                 return  0;
+            //缺少了判断两端，左大右边小的状况了--这个只能判断左小右大---因为我一直以为全大于的情况就要归为下面来处理了
+            if(m>height[left]){
+                 left = right+1;
+            }else  {
+                sum+=wide*m-sum_b;
+                right=left;
+            }
+            sum_b=0;
+            wide=0;
+            //里面太简单了---忽略了一些东西---连续的时候---也就是说这个只算间隔一个---因为前面已经判断多个的情况了
+            while (height[right] > height[left]) {
+                //更新次高吗？---不确定次高是多少
+                sum_b=height[left];//计算高度
+                right++;
+                left++;
+                wide=1;
+                if(left==height.length-1){
+                    break;
+                }
+            }
+            if(left==height.length){
+               left--;
+            }
+            //开始计算吗？---计算了
+            if(height[right-1]>=height[left]){
+                sum+=wide*height[left]-sum_b;
+            }else {
+                sum+=wide*height[right-1]-sum_b;
+            }
+            if(left!=height.length){
+                right=left;
+            }
+
+        }
+
+    return sum;
+    }
+
+    public static void main(String[] args) {
+        int [] nu={3, 1, 4, 2};
+        int m=trap(nu);
+        System.out.println(m);
+    }
+
+}
