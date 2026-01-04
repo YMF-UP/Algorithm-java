722a11d2e5f8ba1dad3ffb4ee47c965fee91a5b7
YMF-UP <2571853049@qqcom>
2026-01-04 11:43:54 +0800

    这才是正确的dp

--- src/main/java/com/practice/algorithm/leetcode/Hot100/hard/p42_trap/Dp.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/hard/p42_trap/Dp.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Dp {
 
     /// 这是错误的dp学习思路---我正在改变和学习
-    //动态规划思路吗？----water=min-now---然后更新
+   /* //动态规划思路吗？----water=min-now---然后更新
     public static int trap(int[] height) {
           int []water=new int[height.length-1];
         Arrays.fill(water,0);
@@ -43,8 +43,27 @@ public class Dp {
             sum+=water[i];
         }
         return sum;
-    }
+    }*/
+    public static int trap(int[] height){
+        int []left=new int[height.length];
+        int []right=new int[height.length];
+        int []water=new int[height.length];
+        int sum=0;
+        left[0]=height[0];
+        right[height.length-1]=height[height.length-1];
+        for (int i = 1; i < left.length; i++) {
+            left[i]=Math.max(left[i-1],height[i]);
+        }
+        for (int i = right.length-2; i >= 0; i--) {
+            right[i]=Math.max(right[i+1],height[i]);
+        }
+        for (int i = 0; i < water.length; i++) {
+            water[i]=Math.min(left[i],right[i])-height[i];
+            sum+=water[i];
+        }
 
+        return  sum;
+    }
 
     public static void main(String[] args) {
         int [] nu={5,4,1,2};
