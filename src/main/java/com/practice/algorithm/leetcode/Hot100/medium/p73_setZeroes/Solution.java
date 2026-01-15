e494745c6ea9b479c6f2d6350b8ed193ab77b7ae
YMF-UP <2571853049@qqcom>
2026-01-15 17:00:12 +0800

    我觉着确实要看题解这种学习了

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p73_setZeroes/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p73_setZeroes/Solution.java
@@ -3,7 +3,7 @@ package com.practice.algorithm.leetcode.Hot100.medium.p73_setZeroes;
 public class Solution {
 
     public void setZeroes(int[][] matrix) {
-       //只记录行和列那也是o(m+n)---但是我对二维数组确实不熟悉
+      /* //只记录行和列那也是o(m+n)---但是我对二维数组确实不熟悉
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 // 关键就是判断这个是不是初始0还被改变的0,
@@ -11,6 +11,55 @@ public class Solution {
                 if(matrix[i][j]==0){
 
                 }
+            }
+        }*/
+
+        //先处理第一行和第一列目的是为了之后被代替了，但是没有把对应的及时变为0---要标记
+        //但是为什么处理的时候要跳过第一行第一列呢？---担心存储的被改变了---不对啊这样
+        //标记的是除第一行和第一列其余为0的吗？---第一行第一列是的,第一行和第一列单独标记
+        boolean firstRow=false;
+        boolean firstList=false;
+        for (int i = 0; i < matrix.length; i++) {
+            if(matrix[i][0]==0){
+                firstList=true;
+            }
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            if(matrix[0][i]==0){
+                firstRow=true;
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        //然后处理第一行和第一列---已经是标记的了---行处理行，列处理列--但是[0][0]不处理
+        for (int i = 1; i < matrix.length; i++) {
+            if(matrix[i][0]==0){
+                for (int j = 0; j < matrix[i].length; j++) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix[0].length; i++) {
+            if(matrix[0][i]==0){
+                for (int j = 0; j < matrix.length; j++) {
+                    matrix[j][i]=0;
+                }
+            }
+        }
+        if(firstRow){
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i]=0;
+            }
+        }
+        if(firstList){
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0]=0;
             }
         }
     }
