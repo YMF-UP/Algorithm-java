f2095ac04ac4d5817651002d9eacd68e679abe90
YMF-UP <2571853049@qqcom>
2026-01-16 17:11:44 +0800

    步长这个做法,我确实不太会。只能参考了

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p54_spiralOrder/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p54_spiralOrder/Solution.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Solution {
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-     /*   List<Integer> list=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         // 碰到尽头就转弯---重点就是尽头---其实就是每转两次弯一次就少一个，除了第一次
         // 特殊处理第一行
         for (int i = 0; i < matrix[0].length; i++) {
@@ -36,13 +36,13 @@ public class Solution {
         int statueList=matrix[0].length-1;
         while (flagRow!=0&&flagList!=0){
             if(k%2!=0){
-               *//* for (int i = statueRow; i <=flagRow; i++) {
+                for (int i = statueRow; i <=flagRow; i++) {
                     list.add(matrix[i][statueList]);
                 }
                 statueList++;
                 for (int j = statueList; j>=flagList; j--) {
                         list.add(matrix[flagRow][j]);
-                }*//*
+                }
                 for (  ; statueRow <=flagRow ; statueRow++) {
                     list.add(matrix[statueRow][statueList]);
                 }
@@ -59,8 +59,8 @@ public class Solution {
             }
 
         }
-        return list;*/
-        ///  还是想不到啊怎么--有时候是差一点--有时候差了好多
+        return list;
+      /*  ///  还是想不到啊怎么--有时候是差一点--有时候差了好多
         List<Integer> res=new ArrayList<>();
         int Dir[][] ={{0,1},{1,0},{0,-1},{-1,0}};//这个确实挺牛的
         int dir=0;
@@ -69,7 +69,7 @@ public class Solution {
         int n=matrix[0].length;
         for (int k = 0; k <m*n ; k++) {
 
-           /* // 具体处理--但是怎么确定这个方向的加减呢?---怎么往下走呢?---所以Dir方向是开始又是增加的
+           *//* // 具体处理--但是怎么确定这个方向的加减呢?---怎么往下走呢?---所以Dir方向是开始又是增加的
             // 怎么知道是行还是列变化呢?---dir决定的---好像有点问题这个判断条件
             while (i>=0&&i<m&&j>=0&&j<n&&matrix[i][j]!=Integer.MAX_VALUE){
               res.add(matrix[i][j]);
@@ -83,7 +83,7 @@ public class Solution {
             dir++;
             dir%=4;
             i+=Dir[dir][0];
-            j+=Dir[dir][1];*/
+            j+=Dir[dir][1];*//*
             res.add(matrix[i][j]);
             matrix[i][j]=Integer.MAX_VALUE;
             int nexti=i+Dir[dir][0];
@@ -95,7 +95,54 @@ public class Solution {
             j+=Dir[dir][1];
 
         }
-        return res;
+        return res;*/
+
+        /// 其实步长也能做---比我之前想的状态处理要简单一些
+       /* List<Integer> res=new ArrayList<>();
+        int rows=matrix.length;
+        int lists=matrix[0].length;
+        int flagorow=0;
+        int flagList=0;
+        while (rows>=0||lists>=0){
+            // 步长的话,问题也是转向
+            for ( flagList = 0; flagList < rows; flagList++) {
+                res.add(matrix[flagorow][flagList]);
+            }
+            for (int flaglist = 0; flaglist < ; flaglist++) {
+
+            }
+
+        }
+        return res;*/
+
+//        class Solution {
+//            private static final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 右下左上
+//
+//            public List<Integer> spiralOrder(int[][] matrix) {
+//                int m = matrix.length;
+//                int n = matrix[0].length;
+//                int size = m * n;
+//                List<Integer> ans = new ArrayList<>(m * n); // 预分配空间
+//                int i = 0;
+//                int j = -1; // 从 (0, -1) 开始
+//                for (int di = 0; ans.size() < size; di = (di + 1) % 4) {
+//                    for (int k = 0; k < n; k++) { // 走 n 步（注意 n 会减少）
+//                        i += DIRS[di][0];
+//                        j += DIRS[di][1]; // 先走一步
+//                        ans.add(matrix[i][j]); // 再加入答案
+//                    }
+//                    int tmp = n;
+//                    n = m - 1; // 减少后面的循环次数（步数）
+//                    m = tmp;
+//                }
+//                return ans;
+//            }
+//        }
+//
+//        作者：灵茶山艾府
+//        链接：https://leetcode.cn/problems/spiral-matrix/solutions/2966229/liang-chong-fang-fa-jian-ji-gao-xiao-pyt-4wzk/
+//        来源：力扣（LeetCode）
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
     }
 
     public static void main(String[] args) {
