db5d1a847bf0f4a435f8f01a3d78a7a38ab3d022
YMF-UP <2571853049@qqcom>
2026-01-23 19:51:09 +0800

    果然还是不行--第一直觉还是太差

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p48_rotate/Solution.java
@@ -0,0 +1,22 @@
+package com.practice.algorithm.leetcode.Hot100.medium.p48_rotate;
+
+public class Solution {
+
+    public void rotate(int[][] matrix) {
+        // 我的想法就是旋转---那就开始一个接一个旋转呗---n-1最外层，随后减2--嵌套一个循环--用于每一个的替换
+        // 重点就是内层的循环---当前位置加移动位置随后求行的余数
+        // 对角线方式处理层数---0,0-1,1
+        // 不行---调换还是太难--也就是内部的那个循环
+       /* int n=matrix.length-1;
+         while (n!=0){
+             int
+             for (int i = 0; i < ; i++) {
+
+             }
+         }*/
+
+
+
+    }
+
+}
