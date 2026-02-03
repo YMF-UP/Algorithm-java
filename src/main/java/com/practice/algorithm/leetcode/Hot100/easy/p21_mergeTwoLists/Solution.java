a53b06a54ee9b70cd13ac3788e412ce7f870e3ee
YMF-UP <2571853049@qqcom>
2026-02-03 11:03:03 +0800

    加了一个简略的解法以及一个之前死活想不到的递归解法

--- src/main/java/com/practice/algorithm/leetcode/Hot100/easy/p21_mergeTwoLists/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/easy/p21_mergeTwoLists/Solution.java
@@ -72,5 +72,50 @@ public class Solution {
 			newNode.next=list1;
 		}
 		return  now;
+
+
+		/*class Solution {
+			public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+				ListNode dummy = new ListNode(); // 用哨兵节点简化代码逻辑
+				ListNode cur = dummy; // cur 指向新链表的末尾
+				while (list1 != null && list2 != null) {
+					if (list1.val < list2.val) {
+						cur.next = list1; // 把 list1 加到新链表中
+						list1 = list1.next;
+					} else { // 注：相等的情况加哪个节点都是可以的
+						cur.next = list2; // 把 list2 加到新链表中
+						list2 = list2.next;
+					}
+					cur = cur.next;
+				}
+				cur.next = list1 != null ? list1 : list2; // 拼接剩余链表
+				return dummy.next;
+			}
+		}
+
+		作者：灵茶山艾府
+		链接：https://leetcode.cn/problems/merge-two-sorted-lists/solutions/2373691/liang-chong-fang-fa-die-dai-di-gui-pytho-wf75/
+		来源：力扣（LeetCode）
+		著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+
+
+		/*class Solution {
+			public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+				if (list1 == null) return list2; // 注：如果都为空则返回空
+				if (list2 == null) return list1;
+				if (list1.val < list2.val) {
+					list1.next = mergeTwoLists(list1.next, list2);
+					return list1;
+				}
+				list2.next = mergeTwoLists(list1, list2.next);
+				return list2;
+			}
+		}
+
+		作者：灵茶山艾府
+		链接：https://leetcode.cn/problems/merge-two-sorted-lists/solutions/2373691/liang-chong-fang-fa-die-dai-di-gui-pytho-wf75/
+		来源：力扣（LeetCode）
+		著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+
 	}
 }
