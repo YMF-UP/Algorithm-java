5f8b6cbc2023927e854092dd5e971caef9c35087
YMF-UP <2571853049@qqcom>
2026-02-03 15:38:43 +0800

    这个优秀的递归代码多学习学习

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p2_addTwoNumbers/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p2_addTwoNumbers/Solution.java
@@ -65,6 +65,33 @@ public class Solution {
 	       cur=newNode;
        }*/
        return dummy.next;
+
+		/*class Solution {
+			public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+				return addTwo(l1, l2, 0);
+			}
+
+			// l1 和 l2 为当前遍历的节点，carry 为进位
+			private ListNode addTwo(ListNode l1, ListNode l2, int carry) {
+				if (l1 == null && l2 == null) { // 递归边界
+					return carry != 0 ? new ListNode(carry) : null; // 如果进位了，就额外创建一个节点
+				}
+				if (l1 == null) { // 如果 l1 是空的，那么此时 l2 一定不是空节点
+					l1 = l2;
+					l2 = null; // 交换 l1 与 l2，保证 l1 非空，从而简化代码
+				}
+				int sum = carry + l1.val + (l2 != null ? l2.val : 0); // 节点值和进位加在一起
+				l1.val = sum % 10; // 每个节点保存一个数位（直接修改原链表）
+				l1.next = addTwo(l1.next, (l2 != null ? l2.next : null), sum / 10); // 进位
+				return l1;
+			}
+		}
+
+		作者：灵茶山艾府
+		链接：https://leetcode.cn/problems/add-two-numbers/solutions/2327008/dong-hua-jian-ji-xie-fa-cong-di-gui-dao-oe0di/
+		来源：力扣（LeetCode）
+		著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+
 	}
 
 }
