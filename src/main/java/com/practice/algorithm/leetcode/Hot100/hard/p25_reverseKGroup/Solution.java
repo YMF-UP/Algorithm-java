b72db851af33a7249cad781d7e2866508867fc39
YMF-UP <2571853049@qqcom>
2026-02-24 14:15:58 +0800

    这个确实忽略了太多条件--以及可优化的点

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/hard/p25_reverseKGroup/Solution.java
@@ -0,0 +1,66 @@
+package com.practice.algorithm.leetcode.Hot100.hard.p25_reverseKGroup;
+
+import com.practice.algorithm.model.ListNode;
+
+
+
+/**
+ * LeetCode 25.
+ * 题目描述：
+ * 示例：
+ * 解题思路：1.到还是和24差不多吗，对吧
+ * 时间复杂度：O(...)
+ * 空间复杂度：O(...)
+ */
+
+
+public class Solution {
+
+	public static ListNode reverseKGroup(ListNode head, int k) {
+        //这个是分组处理--本来是想到反转链表的---但是不确定k是否存在吧--或者说先判断?
+        //感觉就还是先判断然后反转--短于k的就不反转了
+		//有一点的逻辑思考啊--k范围内做转置--但是多两个点考虑:1.首位 2.跟随--也包括了返回值
+		ListNode dummy=new ListNode(0,head);
+		ListNode node=dummy;
+		int count=0;
+		while (count==0&&dummy.next!=null){
+			ListNode nodek=dummy;
+			count=k;
+			//一定要先判断是否在k以内吗?
+            while(count!=0&&nodek.next!=null){
+				nodek=nodek.next;
+				count--;
+            }
+			ListNode next=nodek.next;
+			ListNode node1=dummy.next;
+			ListNode node2=node1.next;
+			if(node2==null){
+				break;
+			}
+			ListNode node3;
+			// 提前处理一下首尾吗?--不提前会怎么样
+			if (count==0){
+				node1.next=next;
+				dummy.next=nodek;
+				dummy=node1;
+			}
+			//开始反转
+			while (count==0&&node2!=next){
+				node3=node2.next;
+			    node2.next=node1;
+			    node1=node2;
+			    node2=node3;
+			}
+		}
+		return node.next;
+	}
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		ListNode node=reverseKGroup(head,2);
+	}
+}
