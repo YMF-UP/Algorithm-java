e3a19262fbf85eda8232f46ace23e297a68a962d
YMF-UP <2571853049@qqcom>
2026-02-26 14:56:55 +0800

    写出来了--但也暴露了很多东西--最重要的一点就是我喜欢逃避,或者说我忍不住去逃避,这才导致写得很慢。总体来讲--作为递归写法算是简单同时也有自己的思考

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p148_sortList/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p148_sortList/Solution.java
@@ -24,14 +24,38 @@ public class Solution {
 		}
 		return slow;
 	}
+	private ListNode merge(ListNode list1,ListNode list2){
+		/// 之前没怎么仔细看题解--虽然自己也写出来了--但终究不是最优解
+		ListNode dummy=new ListNode(0,null);
+		ListNode cur=dummy;
+			while(list1!=null&&list2!=null){
+				if(list1.val<list2.val){
+					dummy.next=list1;
+					list1=list1.next;
+				}else {
+					dummy.next=list2;
+					list2=list2.next;
+				}
+				dummy=dummy.next;
+			}
+			 if (list1!=null) {
+				dummy.next=list1;
+			 }
+			 else {
+                dummy.next=list2;
+			 }
 
+		return cur.next;
+	}
+
+	/* 并不对的快排--至少对这个题来说--或者说链表不能用快排
 	private ListNode quickSort(ListNode head){
 
 
 		return head;
-	}
+	}*/
 	public ListNode sortList(ListNode head) {
-		if(head==null||head.next==null){
+		/*if(head==null||head.next==null){
 			return head;
 		}
         ListNode dummy=head;
@@ -44,6 +68,19 @@ public class Solution {
 		//左右需要写两个不一样的函数排序--不能通用因为
 		//但是好像很难递归
 		ListNode  middle=medium(head);//找中间位置
-		return dummy;
+		return dummy;*/
+		/// 归并算法处理链表的排序--或者说归并处理的就是链表
+		/// 题解分了迭代和递归---我还真有点懵--而且我看题解还挺长的
+		/// 递归好像更好理解一些---一直分两段直到只剩一个--返回后怎么做(这个就是我困扰的点)
+		/// 一层左一层右--像二叉树一样--先走左,后返回到父节点走右--感觉像是简单的中序遍历啊
+		if(head==null||head.next==null){
+			return head;
+		}
+		ListNode middle=medium(head);//也可以称之为父节点了
+        ListNode right=sortList(middle.next);
+		middle.next=null;
+        ListNode left=sortList(head);
+		/// 然后开始合并--从1个数开始也就是返回到父节点--就是返回这个有点不太懂怎么处理,还是逻辑问题吗?
+		return merge(left,right);
 	}
 }
