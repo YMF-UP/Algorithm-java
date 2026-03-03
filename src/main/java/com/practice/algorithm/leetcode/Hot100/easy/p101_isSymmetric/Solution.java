0bc579626438ab5de0fd9e186b973204f4478cb3
YMF-UP <2571853049@qqcom>
2026-03-03 19:50:06 +0800

    算不上难,就是需要精简一下,主要还是递归--当然迭代也能写

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/easy/p101_isSymmetric/Solution.java
@@ -0,0 +1,36 @@
+package com.practice.algorithm.leetcode.Hot100.easy.p101_isSymmetric;
+
+import com.practice.algorithm.model.TreeNode;
+
+
+/**
+ * LeetCode 101. 轴对称
+ * 题目描述：
+ * 示例：
+ * 解题思路：
+ * 时间复杂度：O(...)
+ * 空间复杂度：O(...)
+ */
+
+public class Solution {
+
+	private boolean  is(TreeNode left,TreeNode right){
+		if(left==null&&right==null){
+			return true;
+		}
+		if(left==null||right==null||left.val!=right.val){
+			return false;
+		}
+		//关键是两个都要判定---那怎么return呢?--或者说,判定终止条件
+		boolean isLeft= is(left.left,right.right);
+		boolean isRight=is(left.right,right.left);
+		return (isRight&&isLeft);
+	}
+
+	public boolean isSymmetric(TreeNode root) {
+        //有意思啊--递归和迭代
+		//得传两个root节点吧--当前层--层序遍历吗?--哈哈哈
+		return is(root.left,root.right);
+	}
+
+}
