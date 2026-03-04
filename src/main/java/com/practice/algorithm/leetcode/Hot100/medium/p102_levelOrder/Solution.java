5e18884c51af642c5cbe808c5e63b08775bcd0c3
YMF-UP <2571853049@qqcom>
2026-03-04 17:27:25 +0800

    果然啊

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p102_levelOrder/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p102_levelOrder/Solution.java
@@ -18,24 +18,30 @@ import java.util.*;
 public class Solution {
 
 	public List<List<Integer>> levelOrder(TreeNode root) {
+		if(root==null)
+		{
+			return List.of();
+		}
 		List<List<Integer>> order=new ArrayList<>();
-
 		Queue<TreeNode> queue=new LinkedList<>();
-//		now.add(root);
+		queue.add(root);
 		//其实就是bfs
-		while (root!=null||!queue.isEmpty()){
+		while (!queue.isEmpty()){
             //现在的难点就是在于判断是不是在同一层--能插null的
-			List<Integer> level=new ArrayList<>();
-//			root=now.poll();
-			while (root!=null) {
-
-			    queue.add(root.left);
-				queue.add(root.right);
-				level.add(root.val);
+			int size=queue.size();
+			List<Integer> level=new ArrayList<>(size);
+			while (size-->0) {
 				root=queue.poll();
+                if(root.left!=null)
+				{
+			    queue.add(root.left);}
+				if(root.right!=null)
+				{
+				queue.add(root.right);}
+				level.add(root.val);
+
 			}
 			order.add(level);
-
 		}
 		return  order;
 	}
