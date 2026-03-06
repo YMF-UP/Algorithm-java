914c2dfbca25b18a3214067819233488c199e1dc
YMF-UP <2571853049@qqcom>
2026-03-06 16:26:37 +0800

    只能判定成false--哈哈哈,我服了。哪里出错了啊?

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p98_isValidBST/Solution.java
@@ -0,0 +1,44 @@
+package com.practice.algorithm.leetcode.Hot100.medium.p98_isValidBST;
+
+import com.practice.algorithm.model.TreeNode;
+
+
+
+/**
+ * LeetCode 98. 判断搜索树
+ * 题目描述:
+ * 示例:
+ * 解题思路:1.就是很简单的判定---中序遍历是否可行就行了
+ *        2.问题在于怎么判断
+ * 时间复杂度:O(...)
+ * 空间复杂度:O(...)
+ */
+
+
+public class Solution {
+
+	private boolean dfs(TreeNode root,int val){
+		if(root.left==null&&root.right==null){
+			if(root.val>=val){
+				return false;
+			}else {
+				return  true;
+			}
+		}
+		boolean left=true,right=true;
+		if(root.left!=null){
+			 left=dfs(root.left,root.val);
+		}
+		if(root.right!=null){
+		 right=dfs(root.right,root.val);
+		}
+		return left&&right;
+
+	}
+
+	public boolean isValidBST(TreeNode root) {
+         //要依赖于前一个的中序遍历得到的数值
+         return dfs(root,root.val);
+
+	}
+}
