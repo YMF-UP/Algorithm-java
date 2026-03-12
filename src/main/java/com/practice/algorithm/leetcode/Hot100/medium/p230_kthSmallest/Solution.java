c4a19fe0314cc38a86eb7f079cd0f6acc06b0b13
YMF-UP <2571853049@qqcom>
2026-03-12 16:05:41 +0800

    这个是题解--有些启发

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p230_kthSmallest/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p230_kthSmallest/Solution.java
@@ -36,4 +36,31 @@ public class Solution {
 		return  result;
 	}
 
+	/*class Solution {
+		private int k;
+
+		public int kthSmallest(TreeNode root, int k) {
+			this.k = k;
+			return dfs(root);
+		}
+
+		private int dfs(TreeNode node) {
+			if (node == null) {
+				return -1; // 题目保证节点值非负，用 -1 表示没有找到
+			}
+			int leftRes = dfs(node.left);
+			if (leftRes != -1) { // 答案在左子树中
+				return leftRes;
+			}
+			if (--k == 0) { // 答案就是当前节点
+				return node.val;
+			}
+			return dfs(node.right); // 右子树会返回答案或者 -1
+		}
+	}
+
+	作者：灵茶山艾府
+	链接：https://leetcode.cn/problems/kth-smallest-element-in-a-bst/solutions/2952810/zhong-xu-bian-li-pythonjavaccgojsrust-by-wc02/
+	来源：力扣（LeetCode）
+	著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
 }
