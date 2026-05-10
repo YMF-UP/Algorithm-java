98821917f07a9c021deae25ddca2a40f06f45102
YMF-UP <2571853049@qqcom>
2026-05-10 22:38:06 +0800

    bfs确实是想不到的一个思路

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p994_orangesRotting/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p994_orangesRotting/Solution.java
@@ -1,6 +1,8 @@
 package com.practice.algorithm.leetcode.Hot100.medium.p994_orangesRotting;
 
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 /**
  * LeetCode 944.
@@ -14,7 +16,7 @@ package com.practice.algorithm.leetcode.Hot100.medium.p994_orangesRotting;
 
 public class Solution {
 
-	private int dfs(int [][] grid,int time,int i,int j){
+	/*private int dfs(int [][] grid,int time,int i,int j){
 		if(grid[i][j]==2){
 			return time;
 		}
@@ -52,6 +54,50 @@ public class Solution {
 		}
 		return max;
 	}
+*/
 
-
+	public int orangesRotting(int[][] grid) {
+		int countnum=0;
+		int row=grid.length-1;
+		int line=grid[0].length-1;
+		Queue<int[]> queue=new ArrayDeque<>();
+		//先统计坏橘子位置,方便一起去感染,作为第一层
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if(grid[i][j]==2){
+					queue.add(new int[]{i,j});
+				}
+				if(grid[i][j]==1){
+					countnum++;//判断最终是否有空白无法到达的,厉害
+				}
+			}
+		}
+		//然后就是bfs了,但是怎么记时间呢?
+		//bfs需要去单独写一个函数吗?
+		//第一层全部出来完--然后算时间和感染数,随后第二层
+		//一开始我还想着说怎么处理四个方向呢--原来是这样啊
+		int time=0;
+		int [][] direction=new int[][]{{-1,0},{0,-1},{1,0},{0,1}};
+		while (countnum>0&&!queue.isEmpty()){
+			//要有两个循环吗?
+			int num=queue.size();//当前层有几个节点
+			for (int i = 0; i < num; i++) {
+				int [] cur=queue.poll();
+				//开始走四方
+				for (int j = 0; j < direction.length; j++) {
+					int k=cur[0]+direction[j][0];
+					int m=cur[1]+direction[j][1];
+					//或还是并呢?
+					if(k>=0&&k<=row&&m>=0&&m<=line&&grid[k][m]==1)
+					{
+						grid[k][m]=2;
+						queue.add(new int[]{k,m});
+						countnum--;
+					}
+				}
+			}
+             time++;
+		}
+		return countnum >0? -1:time;
+	}
 }
