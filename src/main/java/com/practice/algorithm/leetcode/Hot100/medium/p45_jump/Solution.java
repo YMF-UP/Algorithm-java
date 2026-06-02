bcc2f3163f2f1466464bb6fa1cf29c31722aeb63
YMF-UP <2571853049@qqcom>
2026-06-02 15:31:52 +0800

    有问题啊,但是我不值得哪里出错了,模拟吗？还是贪心有问题

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p45_jump/Solution.java
@@ -0,0 +1,37 @@
+package com.practice.algorithm.leetcode.Hot100.medium.p45_jump;
+
+public class Solution {
+
+	public int jump(int[] nums) {
+	   if(nums.length==1){
+		   return 0;
+	   }
+       int min=0;
+	   int n=nums.length-1;
+	   int cur=0;
+	   //这个就需要回溯了吧,因为一定可以走到的话,那肯定需要最大的几步
+	   //每一次走最大的--还是说判断下一步走哪几步是走的最大的
+	   //上一题是按照最大的步数能不能当前下标--这个真的很妙啊
+       //那这一题呢?
+		for (int i:nums){
+			//首先就是判断是不是能达到
+			if(cur+i>=n){
+				min+=1;
+				break;
+			}
+			int max=0;
+			//就是判断cur应该是哪个了--下一步去哪个下标更好
+			for (int j = nums[cur]; j >=1 ; j--) {
+				//好像不太对,cur是当前下标位置,最后还是要有cur的啊,还是说不要cur了,必须要啊,不然怎么走循环啊
+				//max=Math.max(nums[j]+cur,max);
+				if(j+nums[j+cur]>max){
+					max=nums[j+cur];
+					cur=j;
+				}
+			}
+            min+=1;
+		}
+	   return min;
+	}
+
+}
