808f4355cccf9ba419a1e7a6bb13d260e3710aba
YMF-UP <2571853049@qqcom>
2026-06-10 10:17:11 +0800

    还是思考又漏洞--或者说没建立起来一个规范

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p64_minPathSum/Solution.java
@@ -0,0 +1,16 @@
+package com.practice.algorithm.leetcode.Hot100.medium.p64_minPathSum;
+
+public class Solution {
+
+	public int minPathSum(int[][] grid) {
+
+		//这个做出来的这么多吗?--为社么是多维啊?看不出来为什么?
+		//就像是怎么说呢---还是要都走一遍吧,不然怎么找最小的呢?
+		//如果都走以便怎么用到dp了呢?
+		//dp要记录什么状态--我需要记录什么状态?为什么需要这些状态?
+		//肯定不能用贪心--当前最优不是代表全局最优,但是全遍历是可以的吗?
+		//还是分析不出来要记录什么状态--需要什么状态?
+
+	}
+
+}
