d2a139aa1f6a4c314c60ae99881d466a2991162c
YMF-UP <2571853049@qqcom>
2026-06-23 09:47:18 +0800

    上一次看完题解去尝试忘记什么时候了,也是没写出来,这次就再写一下看看

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p215_findKthLargest/First.java
@@ -0,0 +1,15 @@
+package com.practice.algorithm.leetcode.Hot100.medium.p215_findKthLargest;
+
+public class First {
+
+	/*public int findKthLargest(int[] nums, int k) {
+		//第一次复刻--说实话有点不太理解了
+		//我记得是快排的思想演变,但是是新的算法思路
+		//我真忘了,时间复杂度保持在o(n)--很难想
+		//
+
+	}*/
+
+
+
+}
