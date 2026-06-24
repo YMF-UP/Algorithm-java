1b88b4d141f9b298b5ed7d658b1a0a97dc1ff86f
YMF-UP <2571853049@qqcom>
2026-06-24 11:12:49 +0800

    还是没写出来--看题解是很简单的,还是不行啊

--- src/main/java/com/practice/algorithm/leetcode/Hot100/hard/p124_maxPathSum/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/hard/p124_maxPathSum/Solution.java
@@ -15,7 +15,7 @@ import com.practice.algorithm.model.TreeNode;
  */
 
 public class Solution {
-    private int maxSum=Integer.MIN_VALUE;
+    /*private int maxSum=Integer.MIN_VALUE;
 	public int maxPathSum(TreeNode root) {
          //现在的问题就是方向---每个节点只能出现一次
 		 //根左--根右--左根右,只有这三个方向了
@@ -36,5 +36,34 @@ public class Solution {
 		int right=sum(root.right);
         maxSum=Math.max(maxSum,left+right+root.val);//v形,转弯的--不仅仅,还要那种全负的
 		return Math.max(Math.max(left,right)+root.val,0);//链式
+	}*/
+    private  static int max=Integer.MIN_VALUE;
+	private static void dfs(TreeNode root,int cur){
+		if(root==null){
+			return;
+		}
+		//开始加,在开始前吗?--从后往前吗?
+		dfs(root.left,cur);
+		if(cur<=0){
+			//不对啊这样.应该还需要一个变量,怎么去遍历右边呢?
+			cur=root.val;
+		}else {
+			cur+=root.val;
+		}
+		//这个放的对吗?
+		max=Math.max(max,cur);
+		dfs(root.right,cur);
+	}
+
+	public int maxPathSum(TreeNode root) {
+        if(root==null){
+			return 0;
+        }
+		//树都是递归,递归我都开始有点遗忘了--思路就是小于等于0的时候开始更换
+		//问题的关键应该是递归逻辑,那个遍历树
+		//状态--递归中需要的状态
+		dfs(root,0);
+		return max;
+
 	}
 }
