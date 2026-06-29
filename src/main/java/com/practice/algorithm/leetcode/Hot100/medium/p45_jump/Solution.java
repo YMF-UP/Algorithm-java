3c7aa43a25d41da19b240ddfcba46dc8541ddf98
YMF-UP <2571853049@qqcom>
2026-06-29 09:24:45 +0800

    我记得当时也没写出来,这次再看看,很久了不过

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p45_jump/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p45_jump/Solution.java
@@ -6,17 +6,17 @@ public class Solution {
 	   if(nums.length==1){
 		   return 0;
 	   }
-       int min=0;
+       int min=1;
 	   int n=nums.length-1;
 	   int cur=0;
 	   //这个就需要回溯了吧,因为一定可以走到的话,那肯定需要最大的几步
 	   //每一次走最大的--还是说判断下一步走哪几步是走的最大的
 	   //上一题是按照最大的步数能不能当前下标--这个真的很妙啊
        //那这一题呢?
-		for (int i:nums){
+		while (cur<=n)
+		{
 			//首先就是判断是不是能达到
-			if(cur+i>=n){
-				min+=1;
+			if(cur+nums[cur]>=n){
 				break;
 			}
 			int max=0;
@@ -25,8 +25,8 @@ public class Solution {
 				//好像不太对,cur是当前下标位置,最后还是要有cur的啊,还是说不要cur了,必须要啊,不然怎么走循环啊
 				//max=Math.max(nums[j]+cur,max);
 				if(j+nums[j+cur]>max){
-					max=nums[j+cur];
-					cur=j;
+					max=nums[j+cur]+j;
+					cur+=j;
 				}
 			}
             min+=1;
