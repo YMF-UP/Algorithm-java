1e56787f1478c5923ca896ca7bb6593e409fe04c
YMF-UP <2571853049@qqcom>
2026-07-01 13:00:48 +0800

    这个题解有点意思，我想写这个来着，但是不会这个解法

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p78_subsets/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p78_subsets/Solution.java
@@ -58,7 +58,7 @@ public class Solution {
 		return ans;
 	}*/
 
-	private  static  void  dfs(int cur,List<List<Integer>> ans,List<Integer> path,int[] nums){
+	/*private  static  void  dfs(int cur,List<List<Integer>> ans,List<Integer> path,int[] nums){
 		//这个状态就是我要去遍历所有的
 		if(cur==nums.length){
 			ans.add(new ArrayList<>(path));
@@ -83,7 +83,29 @@ public class Solution {
 		return ans;
 
 
+	}*/
+
+
+	class Solution {
+		public List<List<Integer>> subsets(int[] nums) {
+			List<List<Integer>> ans = new ArrayList<>();
+			List<Integer> path = new ArrayList<>();
+			dfs(0, nums, path, ans);
+			return ans;
+		}
+
+		// 枚举选哪个：在下标 i 到 n-1 中选一个数，加到 path 末尾
+		private void dfs(int i, int[] nums, List<Integer> path, List<List<Integer>> ans) {
+			ans.add(new ArrayList<>(path)); // 不选，把当前子集加入答案
+			for (int j = i; j < nums.length; j++) { // 选，枚举选择的数字
+				path.add(nums[j]);
+				dfs(j + 1, nums, path, ans); // 选 nums[j] 意味着 i 到 j-1 都跳过不选，下一个数从 j+1 开始选
+				path.remove(path.size()-1);// path.remove(path.size() - 1);
+			}
+		}
 	}
+
+
 
 
 
