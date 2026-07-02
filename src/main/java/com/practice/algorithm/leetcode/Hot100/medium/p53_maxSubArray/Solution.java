7e010478007f3a3ed10fdf68d9955689d0e602b7
YMF-UP <2571853049@qqcom>
2026-07-02 10:15:05 +0800

    居然没想到dp,虽然这么做法是接近了,但是我确实没想到。我之前已经写很多dp了啊

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p53_maxSubArray/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p53_maxSubArray/Solution.java
@@ -15,74 +15,91 @@ package com.practice.algorithm.leetcode.Hot100.medium.p53_maxSubArray;
 
 public class Solution {
 
-    public static int maxSubArray(int[] nums) {
-         /*  int maxSum=nums[0];
-           //感觉又是一个纯模拟呢---出现两个负数就取消往下走，并计算当前子数组之和
-           //这样来看---栈和队列也能写
-          int flagMinus=0;
-          int index=0;
-          int staticSum=0;
-        while (index<nums.length&&nums[index]<0){
-            index++;
-        }
-          while (index<nums.length){
+//    /*public static int maxSubArray(int[] nums) {
+//         *//*  int maxSum=nums[0];
+//           //感觉又是一个纯模拟呢---出现两个负数就取消往下走，并计算当前子数组之和
+//           //这样来看---栈和队列也能写
+//          int flagMinus=0;
+//          int index=0;
+//          int staticSum=0;
+//        while (index<nums.length&&nums[index]<0){
+//            index++;
+//        }
+//          while (index<nums.length){
+//
+//              if(nums[index]<0){
+//                  flagMinus++;
+//              }
+//              if(flagMinus!=2){
+//                  staticSum+=nums[index];
+//                  index++;
+//                  maxSum=Math.max(staticSum,maxSum);
+//              }else {
+//                  maxSum=Math.max(staticSum,maxSum);
+//                  staticSum=0;
+//                  flagMinus=0;
+//                  while (nums[index]<0){
+//                      index++;
+//                  }
+//              }
+//          }
+//           return maxSum;*//*
+//
+//        ///  关键点是状态---1.什么时候统计?  2.什么位置重新开始?
+//        ///  我觉着我想的是对的---子数组里面将要出现第二个负数就计算,但是我对于第二个问题没有思考好
+//        ///  什么位置开始呢？----负数的下一位---不对，那就可以优化了---直接用和减去第一个负数，然后继续---
+//        ///  不对，负数可能出现在中间位置--所以记录一下
+//        int maxSum=Integer.MIN_VALUE;
+//        int staticSum=0;
+//        int flagMinus=0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if(nums[i]<0){
+//                flagMinus++;
+//
+//            }
+//            if(flagMinus==2){
+//
+//            }
+//        }
+//
+//        return maxSum;
+//
+//      *//*  class Solution {
+//            public int maxSubArray(int[] nums) {
+//                int pre = 0, maxAns = nums[0];
+//                for (int x : nums) {
+//                    pre = Math.max(pre + x, x);
+//                    maxAns = Math.max(maxAns, pre);
+//                }
+//                return maxAns;
+//            }
+//        }
+//
+//        作者：力扣官方题解
+//        链接：https://leetcode.cn/problems/maximum-subarray/solutions/228009/zui-da-zi-xu-he-by-leetcode-solution/
+//        来源：力扣（LeetCode）
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*//*
+//    }
+//
+//    public static void main(String[] args) {
+//        int [] nums={-2,1,-3,4,-1,2,1,-5,4};
+//        System.out.println(maxSubArray(nums));
+//    }*/
 
-              if(nums[index]<0){
-                  flagMinus++;
-              }
-              if(flagMinus!=2){
-                  staticSum+=nums[index];
-                  index++;
-                  maxSum=Math.max(staticSum,maxSum);
-              }else {
-                  maxSum=Math.max(staticSum,maxSum);
-                  staticSum=0;
-                  flagMinus=0;
-                  while (nums[index]<0){
-                      index++;
-                  }
-              }
-          }
-           return maxSum;*/
+    public int maxSubArray(int[] nums) {
 
-        ///  关键点是状态---1.什么时候统计?  2.什么位置重新开始?
-        ///  我觉着我想的是对的---子数组里面将要出现第二个负数就计算,但是我对于第二个问题没有思考好
-        ///  什么位置开始呢？----负数的下一位---不对，那就可以优化了---直接用和减去第一个负数，然后继续---
-        ///  不对，负数可能出现在中间位置--所以记录一下
-        int maxSum=Integer.MIN_VALUE;
-        int staticSum=0;
-        int flagMinus=0;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i]<0){
-                flagMinus++;
-
+        //好像和某一个题很像,但是我忘了
+        //具体思路我还记得,可以说是背下来了
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for (int x:nums){
+            if(sum<=0){
+                sum=0;
             }
-            if(flagMinus==2){
+            sum+=x;
+            max=Math.max(max,sum);
 
-            }
         }
-
-        return maxSum;
-
-      /*  class Solution {
-            public int maxSubArray(int[] nums) {
-                int pre = 0, maxAns = nums[0];
-                for (int x : nums) {
-                    pre = Math.max(pre + x, x);
-                    maxAns = Math.max(maxAns, pre);
-                }
-                return maxAns;
-            }
-        }
-
-        作者：力扣官方题解
-        链接：https://leetcode.cn/problems/maximum-subarray/solutions/228009/zui-da-zi-xu-he-by-leetcode-solution/
-        来源：力扣（LeetCode）
-        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
-    }
-
-    public static void main(String[] args) {
-        int [] nums={-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(nums));
+        return max;
     }
 }
