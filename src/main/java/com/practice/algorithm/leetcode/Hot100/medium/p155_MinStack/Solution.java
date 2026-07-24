5664d306997500b0182409493fbc7186eb61ca75
YMF-UP <2571853049@qqcom>
2026-07-24 15:30:38 +0800

    第一次居然没提交,也不知道过了多久了,确实是不会,第二次确实记得做法但是写不出来

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p155_MinStack/Solution.java
@@ -0,0 +1,43 @@
+package com.practice.algorithm.leetcode.Hot100.medium.p155_MinStack;
+
+public class Solution {
+
+	//哎哟,卧槽,这个是要自己去实现栈的细节---哦哦哦,想起来了,不难啊
+	//408和考研过去了就忘了吗?---就是两个指针啊,但是是循环的吗?循环的话要多考虑一下.
+	/// 并能在常数时间内检索到最小元素的栈。这个怎么处理?---多留一个指针?--如果弹出了呢?怎么处理?
+	/// 还有就是Java里面怎么用指针来着?---通过复用,树和链表都是类型复用
+
+	//主要就是数据类型了,我记得是单独设置了一个,每一次填入的时候,都多填入
+
+	class MinStack {
+
+		//但是怎么定义来着,怎么用,在这个里面?--基础语法不会
+
+		public MinStack() {
+
+			//这样不对吗?---我看树都是这样写的啊
+//            public MinStack top;
+//			public MinStack bottom;
+
+
+		}
+
+		public void push(int val) {
+
+		}
+
+		public void pop() {
+
+		}
+
+		public int top() {
+
+		}
+
+		public int getMin() {
+
+		}
+	}
+
+
+}
