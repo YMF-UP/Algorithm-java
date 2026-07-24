2f540c4f2b4283f408ef5dc90957c2cd732191a4
YMF-UP <2571853049@qqcom>
2026-07-24 15:59:55 +0800

    模板题,就还是看一下得了,还有一点就是对arraylist的语法还是不熟悉

--- src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p102_levelOrder/Solution.java
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p102_levelOrder/Solution.java
@@ -17,32 +17,60 @@ import java.util.*;
 
 public class Solution {
 
-	public List<List<Integer>> levelOrder(TreeNode root) {
-		if(root==null)
-		{
-			return List.of();
-		}
-		List<List<Integer>> order=new ArrayList<>();
-		Queue<TreeNode> queue=new LinkedList<>();
-		queue.add(root);
-		//其实就是bfs
-		while (!queue.isEmpty()){
-            //现在的难点就是在于判断是不是在同一层--能插null的
-			int size=queue.size();
-			List<Integer> level=new ArrayList<>(size);
-			while (size-->0) {
-				root=queue.poll();
-                if(root.left!=null)
-				{
-			    queue.add(root.left);}
-				if(root.right!=null)
-				{
-				queue.add(root.right);}
-				level.add(root.val);
+//	public List<List<Integer>> levelOrder(TreeNode root) {
+//		if(root==null)
+//		{
+//			return List.of();
+//		}
+//		List<List<Integer>> order=new ArrayList<>();
+//		Queue<TreeNode> queue=new LinkedList<>();
+//		queue.add(root);
+//		//其实就是bfs
+//		while (!queue.isEmpty()){
+//            //现在的难点就是在于判断是不是在同一层--能插null的
+//			int size=queue.size();
+//			List<Integer> level=new ArrayList<>(size);
+//			while (size-->0) {
+//				root=queue.poll();
+//                if(root.left!=null)
+//				{
+//			    queue.add(root.left);}
+//				if(root.right!=null)
+//				{
+//				queue.add(root.right);}
+//				level.add(root.val);
+//
+//			}
+//			order.add(level);
+//		}
+//		return  order;
+//	}
 
-			}
-			order.add(level);
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		//就是一个模板题啊,但是我还真不一定能一次写出来
+
+		List<List<Integer>> ans=new ArrayList<>();
+		Deque<TreeNode> deque=new ArrayDeque<>();
+		if(root==null){
+			return ans;
 		}
-		return  order;
+		//这个要先填入吗?
+		deque.addFirst(root);
+		while (!deque.isEmpty()){
+			int num=deque.size();
+			List<Integer> path=new ArrayList<>();
+			for (int i = 0; i < num; i++) {
+				TreeNode cur=deque.pop();
+				path.add(cur.val);
+				if(cur.left!=null){
+					deque.add(cur.left);
+				}
+				if(cur.right!=null){
+					deque.add(cur.right);
+				}
+			}
+			ans.add(new ArrayList<>(path));
+		}
+        return ans;
 	}
 }
