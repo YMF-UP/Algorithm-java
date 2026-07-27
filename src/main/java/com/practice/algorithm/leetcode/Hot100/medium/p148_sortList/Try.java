b3327e4e1abc7970e9e7c41fa658f384e18e4b29
YMF-UP <2571853049@qqcom>
2026-07-27 15:03:01 +0800

    写不出来啊

---
+++ src/main/java/com/practice/algorithm/leetcode/Hot100/medium/p148_sortList/Try.java
@@ -0,0 +1,20 @@
+package com.practice.algorithm.leetcode.Hot100.medium.p148_sortList;
+
+import com.practice.algorithm.model.ListNode;
+
+public class Try {
+
+	public ListNode sortList(ListNode head) {
+		//尝试写一下,迭代--就是归并
+		//思路是相对清晰的
+		/* 1.从长度1开始进行归并,然后加倍---这个要怎么实现来着,循环--要找到两个头
+		   2.具体细节我还真有点没搞清楚
+		   3.以长度作为一个循环,循环里面就是比较和变化--引入了那个函数进行归并
+		   4.但是首尾的处理,这个要先断开记录吗?
+		 */
+
+
+
+	}
+
+}
